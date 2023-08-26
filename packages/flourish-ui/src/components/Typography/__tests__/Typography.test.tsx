52190c07718365dc232153e5b24e4a1eeefe7303
couvq <quentincouvelaire@gmail.com>
2023-08-26 13:30:21 -0500

    changed scss prefix

--- packages/flourish-ui/src/components/Typography/__tests__/Typography.test.tsx
+++ packages/flourish-ui/src/components/Typography/__tests__/Typography.test.tsx
@@ -13,55 +13,55 @@ describe('<Typography />', () => {
     render(<Typography variant="h1">Hello</Typography>)
     const h1 = screen.getByText('Hello')
     expect(h1).toBeInTheDocument()
-    expect(h1).toHaveClass('flourish-h1')
+    expect(h1).toHaveClass('f-h1')
   })
 
   it('h2 is rendered when variant set to h2', () => {
     render(<Typography variant="h2">Hello</Typography>)
     const h2 = screen.getByText('Hello')
     expect(h2).toBeInTheDocument()
-    expect(h2).toHaveClass('flourish-h2')
+    expect(h2).toHaveClass('f-h2')
   })
 
   it('h3 is rendered when variant set to h3', () => {
     render(<Typography variant="h3">Hello</Typography>)
     const h3 = screen.getByText('Hello')
     expect(h3).toBeInTheDocument()
-    expect(h3).toHaveClass('flourish-h3')
+    expect(h3).toHaveClass('f-h3')
   })
 
   it('h4 is rendered when variant set to h4', () => {
     render(<Typography variant="h4">Hello</Typography>)
     const h4 = screen.getByText('Hello')
     expect(h4).toBeInTheDocument()
-    expect(h4).toHaveClass('flourish-h4')
+    expect(h4).toHaveClass('f-h4')
   })
 
   it('h5 is rendered when variant set to h5', () => {
     render(<Typography variant="h5">Hello</Typography>)
     const h5 = screen.getByText('Hello')
     expect(h5).toBeInTheDocument()
-    expect(h5).toHaveClass('flourish-h5')
+    expect(h5).toHaveClass('f-h5')
   })
 
   it('h6 is rendered when variant set to h6', () => {
     render(<Typography variant="h6">Hello</Typography>)
     const h6 = screen.getByText('Hello')
     expect(h6).toBeInTheDocument()
-    expect(h6).toHaveClass('flourish-h6')
+    expect(h6).toHaveClass('f-h6')
   })
 
   it('p is rendered when variant set to p', () => {
     render(<Typography variant="p">Hello</Typography>)
     const p = screen.getByText('Hello')
     expect(p).toBeInTheDocument()
-    expect(p).toHaveClass('flourish-body')
+    expect(p).toHaveClass('f-body')
   })
 
   it('caption is rendered when variant set to caption', () => {
     render(<Typography variant="caption">Hello</Typography>)
     const caption = screen.getByText('Hello')
     expect(caption).toBeInTheDocument()
-    expect(caption).toHaveClass('flourish-caption')
+    expect(caption).toHaveClass('f-caption')
   })
 })
--- packages/flourish-ui/src/components/Typography/index.tsx
+++ packages/flourish-ui/src/components/Typography/index.tsx
@@ -10,15 +10,15 @@ interface TypographyProps {
 
 export const Typography = ({ children, variant = 'p' }: TypographyProps) => (
   <>
-    {variant === 'h1' ? <h1 className="flourish-h1">{children}</h1> : null}
-    {variant === 'h2' ? <h2 className="flourish-h2">{children}</h2> : null}
-    {variant === 'h3' ? <h3 className="flourish-h3">{children}</h3> : null}
-    {variant === 'h4' ? <h4 className="flourish-h4">{children}</h4> : null}
-    {variant === 'h5' ? <h5 className="flourish-h5">{children}</h5> : null}
-    {variant === 'h6' ? <h6 className="flourish-h6">{children}</h6> : null}
-    {variant === 'p' ? <p className="flourish-body">{children}</p> : null}
+    {variant === 'h1' ? <h1 className="f-h1">{children}</h1> : null}
+    {variant === 'h2' ? <h2 className="f-h2">{children}</h2> : null}
+    {variant === 'h3' ? <h3 className="f-h3">{children}</h3> : null}
+    {variant === 'h4' ? <h4 className="f-h4">{children}</h4> : null}
+    {variant === 'h5' ? <h5 className="f-h5">{children}</h5> : null}
+    {variant === 'h6' ? <h6 className="f-h6">{children}</h6> : null}
+    {variant === 'p' ? <p className="f-body">{children}</p> : null}
     {variant === 'caption' ? (
-      <p className="flourish-caption">{children}</p>
+      <p className="f-caption">{children}</p>
     ) : null}
   </>
 )
