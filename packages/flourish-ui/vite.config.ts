d26b40af7a1d3badf43abf0e8ef88ac432a57f5f
couvq <quentincouvelaire@gmail.com>
2023-09-03 14:16:00 -0500

    fixed flourish-ui storybook and configured release scripts

--- packages/flourish-ui/vite.config.ts
+++ packages/flourish-ui/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: /^~(.*)$/,
+        replacement: '$1'
+      }
+    ]
+  },
+  plugins: [react()]
 })
