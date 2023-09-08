7b4a4385bfcd29342492a945df938a8f98b3b2ca
couvq <quentincouvelaire@gmail.com>
2023-09-08 16:42:22 -0500

    added justify-content options to flex component

--- apps/flourish-rapid/src/components/FlexPage/index.tsx
+++ apps/flourish-rapid/src/components/FlexPage/index.tsx
@@ -1,51 +1,83 @@
 import ExamplePage from '@components/ExamplePage'
-import { Flex } from 'flourish-ui'
-import React from 'react'
+import { Flex, Typography } from 'flourish-ui'
+import React, { useState } from 'react'
 import ExampleGroup from '../ExampleGroup'
+import './FlexPage.scss'
 
 interface SquareProps {
   children: React.ReactNode
 }
 
+const flexDirections: string[] = ['col', 'row', 'row-reverse', 'col-reverse']
+
+const justifyContentOptions: string[] = [
+  'start',
+  'center',
+  'space-between',
+  'space-around',
+  'space-evenly'
+]
+
 const Square = ({ children }: SquareProps) => (
-  <div
-    style={{
-      width: '20px',
-      height: '20px',
-      background: 'red',
-      color: 'white'
-    }}
-  >
-    {children}
-  </div>
+  <div className="square">{children}</div>
 )
 
 const FlexPage = () => {
+  const [selectedDirection, setSelectedDirection] = useState<
+    'col' | 'row' | 'row-reverse' | 'col-reverse' | undefined
+  >('col')
+  const [selectedJustifyContent, setSelectedJustifyContent] = useState<
+    | 'start'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly'
+    | undefined
+  >('start')
   return (
     <>
       <ExamplePage exampleName="Flex examples">
-        <ExampleGroup groupName="Column">
-          <Flex direction="col">
-            <Square>1</Square>
-            <Square>2</Square>
-          </Flex>
-        </ExampleGroup>
-        <ExampleGroup groupName="Row">
-          <Flex direction='row'>
-            <Square>1</Square>
-            <Square>2</Square>
-          </Flex>
-        </ExampleGroup>
-        <ExampleGroup groupName="Column Reverse">
-          <Flex direction='col-reverse'>
-            <Square>1</Square>
-            <Square>2</Square>
-          </Flex>
-        </ExampleGroup>
-        <ExampleGroup groupName="Row Reverse">
-          <Flex direction="row-reverse">
-            <Square>1</Square>
-            <Square>2</Square>
+        <ExampleGroup groupName="Basic">
+          <label htmlFor="directions-select">
+            <Typography>Flex direction</Typography>
+          </label>
+          <select
+            name="directions"
+            id="directions-select"
+            value={selectedDirection}
+            // @ts-ignore
+            onChange={(e) => setSelectedDirection(e.target.value)}
+          >
+            {flexDirections.map((direction, i) => (
+              <option value={direction} key={i}>
+                {direction}
+              </option>
+            ))}
+          </select>
+
+          <label htmlFor="jc-select">
+            <Typography>Justify Content</Typography>
+          </label>
+          <select
+            name="jc"
+            id="jc-select"
+            value={selectedJustifyContent}
+            // @ts-ignore
+            onChange={(e) => setSelectedJustifyContent(e.target.value)}
+          >
+            {justifyContentOptions.map((option, i) => (
+              <option value={option} key={i}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <Flex
+            direction={selectedDirection}
+            justifyContent={selectedJustifyContent}
+          >
+            {Array.from(Array(3).keys()).map((number, i) => (
+              <Square key={i}>{number + 1}</Square>
+            ))}
           </Flex>
         </ExampleGroup>
       </ExamplePage>
--- packages/flourish-ui/src/layouts/Flex/index.tsx
+++ packages/flourish-ui/src/layouts/Flex/index.tsx
@@ -8,11 +8,23 @@ enum FlexDirection {
   COL_REVERSE = 'col-reverse'
 }
 
+enum JustifyContent {
+  START = 'start',
+  CENTER = 'center',
+  SPACE_BETWEEN = 'space-between',
+  SPACE_AROUND = 'space-around',
+  SPACE_EVENLY = 'space-evenly'
+}
+
 interface FlexProps {
   /** The content of our flex-box wrapper. */
   children: React.ReactNode
   /** Direction for our flex-box wrapper. */
   direction?: 'row' | 'col' | 'row-reverse' | 'col-reverse'
+  /**  Space distribution and alignment of flex items along the main axis. */
+  justifyContent?: /** eslint-disable */
+  'start' | 'center' | 'space-between' | 'space-around' | 'space-evenly'
+  /** eslint-disable */
 }
 
 const getFlexDirectionClass = (direction?: string): string => {
@@ -30,9 +42,30 @@ const getFlexDirectionClass = (direction?: string): string => {
   }
 }
 
-export const Flex = ({ children, direction }: FlexProps) => {
+const getFlexJustifyContentClass = (justifyContent?: string): string => {
+  switch (justifyContent) {
+    case JustifyContent.START:
+      return 'f-flex-jc-start'
+    case JustifyContent.CENTER:
+      return 'f-flex-jc-center'
+    case JustifyContent.SPACE_BETWEEN:
+      return 'f-flex-jc-space-between'
+    case JustifyContent.SPACE_AROUND:
+      return 'f-flex-jc-space-around'
+    case JustifyContent.SPACE_EVENLY:
+      return 'f-flex-jc-space-evenly'
+    default:
+      return 'f-flex-jc-start'
+  }
+}
+
+export const Flex = ({ children, direction, justifyContent }: FlexProps) => {
   return (
-    <div className={`f-flex ${getFlexDirectionClass(direction)}`}>
+    <div
+      className={`f-flex ${getFlexDirectionClass(
+        direction
+      )} ${getFlexJustifyContentClass(justifyContent)}`}
+    >
       {children}
     </div>
   )
