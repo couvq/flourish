62315720ceb3f5fc4a8d3db2448ea02bd3f377a3
couvq <quentincouvelaire@gmail.com>
2023-09-23 12:03:28 -0500

    playwright config to increase to 3 retries, and capture videos and screenshots

--- apps/flourish-integration-tests/playwright.config.ts
+++ apps/flourish-integration-tests/playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: 2,
+  retries: 3,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -27,7 +27,9 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    video: 'on',
+    screenshot: 'on'
   },
 
   /* Configure projects for major browsers */
